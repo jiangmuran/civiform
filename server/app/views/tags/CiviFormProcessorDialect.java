212a7941e35be67956c824890903bb1adb71dcd1
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2026-02-03 12:09:33 -0800

    wip1

--- server/app/views/tags/CiviFormProcessorDialect.java
+++ server/app/views/tags/CiviFormProcessorDialect.java
@@ -21,9 +21,12 @@ public final class CiviFormProcessorDialect extends AbstractProcessorDialect {
   @Override
   public Set<IProcessor> getProcessors(String dialectPrefix) {
     return Set.of(
+        new AlertElementTagModelProcessor(getPrefix()),
         new ButtonElementTagModelProcessor(getPrefix()),
+        new CheckboxElementTagModelProcessor(getPrefix()),
         new IconElementTagProcessor(getPrefix(), assetsFinder, environment),
         new InputElementTagModelProcessor(getPrefix()),
+        new RadioElementTagModelProcessor(getPrefix()),
         new SelectElementTagModelProcessor(getPrefix()),
         new TextAreaElementTagModelProcessor(getPrefix()));
   }
